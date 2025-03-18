48379e192f313dbec535a450df80e0f9179ad42e
Andres Bedoya <and1res@hotmail.com>
2025-03-18 17:25:28 -0500

    add:Solucion punto 7

--- 07_recursion.js
+++ 07_recursion.js
@@ -1,22 +1,31 @@
 function reduce(arr, fn, initial) {
-    // SOLUTION GOES HERE
-    //console.log(arr,fn,initial);return;
-    if(!arr.length)
+    /*return (function reduceOne(index, value) {
+        if (index > arr.length - 1) return value // end condition
+        return reduceOne(index + 1, fn(value, arr[index], index, arr)) // calculate & pass values to next step  
+    })(0, initial) // IIFE. kick off recursion with initial values*/
+    //let index = initial
+    //console.log(typeof initial === "object",initial=={});return;
+    if(initial.contador == undefined)
     {
-        return initial;
+        initial.contador = 0;
+        initial.ans = {};
     }
-    
-    let check = fn.toString().match(/\{([\s\S]*)\}/)[1].trim();
-    
-    if(check.length != 0)
+    else
     {
-        initial = fn(initial,arr.pop());
-    }  
-    
-    return reduce(arr,(arr,value)=>{
-        arr[value] = arr[value]?++arr[value]:1;
-        return arr;
-    },{});
+        initial.contador++;
+    }    
+    //console.log(arr[initial.contador],initial.contador);
+    //initial.ans = fn(arr[initial.contador],initial.contador);
+    //console.log(initial.contador,arr.length);
+    //return;
+    let value = arr[initial.contador];
+    initial.ans[value] = initial.ans[value]?++initial.ans[value]:1;
+
+    if(initial.contador+1 == arr.length)
+    {
+        return initial.ans;
+    }
+    return reduce(arr,fn,initial);
 }
 /*a = [
     'ut',      'deserunt', 'labore',
